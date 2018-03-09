f5cfc9cef421abace1e7907581d55df64b18f049
Mateo Olarte <mateo.olarte8@gmail.com>
2018-03-09 17:23:43 -0500

    add counter of characters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      countNum: 0
+    }
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.changeCountNum.bind(this)}></textarea>
+        <div className="counter">{this.state.countNum}</div>
       </div>
     );
+  }
+
+  changeCountNum(event) {
+    this.setState({
+      countNum: event.target.value.length
+    })
   }
 }
 
